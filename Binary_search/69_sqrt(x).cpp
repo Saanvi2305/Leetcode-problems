5f4425db47015cb50e5a22564500151174965a4b
Saanvi J Shetty <saanvijshetty@gmail.com>
2026-07-28 23:08:32 +0530

    solved easy problems

---
+++ Binary_search/69_sqrt(x).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if((long long)mid*mid ==x)
+            return mid;
+            else if((long long)mid*mid>x){
+            high=mid-1;}
+            else{
+            ans=mid;
+            low=mid+1;}
+
+        } return ans;
+    }
+};
